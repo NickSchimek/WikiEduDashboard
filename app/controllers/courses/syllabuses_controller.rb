ea7eda6110de107a224b5a37687b302f889ded67
Nick Schimek <cfme00@gmail.com>
2018-07-02 21:22:19 -0700

    update

--- app/controllers/courses/syllabuses_controller.rb
+++ app/controllers/courses/syllabuses_controller.rb
@@ -1,16 +1,16 @@
-# class Courses::SyllabusesController < ApplicationController
-#   before_action :require_permissions, only: :update
+class Courses::SyllabusesController < ApplicationController
+  before_action :require_permissions, only: :update
 
-#   def update
-#     @course = Course.find(params[:id])
-#     handle_syllabus_params
-#     if @course.save
-#       render json: { success: true, url: @course.syllabus.url }
-#     else
-#       render json: { message: I18n.t('error.invalid_file_format') },
-#              status: :unprocessable_entity
-#     end
-#   end
+  def update
+    @course = Course.find(params[:id])
+    handle_syllabus_params
+    if @course.save
+      render json: { success: true, url: @course.syllabus.url }
+    else
+      render json: { message: I18n.t('error.invalid_file_format') },
+             status: :unprocessable_entity
+    end
+  end
 
   private
     def handle_syllabus_params
